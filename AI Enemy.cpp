95f367228edd3779d8dc6963bc90f0901ca85e95
Holocroft <116820276+Holocroft@users.noreply.github.com>
2023-03-31 16:06:18 +0100

    Add files via upload

---
+++ AI Enemy.cpp
@@ -0,0 +1,30 @@
+#include *MagmiteController.h*
+#include 
+
+class Magmite
+{
+	void GENERATED_BODY();
+public:
+	Magmite();
+	~Magmite();
+	MagmiteController();
+
+private:
+	property(EditDefaultOnly, blueprint)
+};
+protected:
+{
+	virtual void 
+}
+
+Magmite::Magmite()
+{
+}
+
+Magmite::~Magmite()
+{
+}
+Magmite::MagmiteController()
+{
+
+}
